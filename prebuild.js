9619a4aaadb95693cb092628b773faca60d7f81d
liyunfeng <yunfeng.li@enlighten.finance>
2023-09-13 14:18:05 +0800

    update github action

--- prebuild.js
+++ prebuild.js
@@ -1,9 +1,9 @@
 const getAnime1Data = require('./script/get-anime1-data');
 const getIqiyiVideoImage = require('./script/get-iqiyi-video-image');
-const getYoukuVideo = require('./script/get-youku-video');
+// const getYoukuVideo = require('./script/get-youku-video');
 
 getAnime1Data();
 getIqiyiVideoImage();
-getYoukuVideo();
+// getYoukuVideo();
 
 
